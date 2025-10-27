593709adea1bbbfb4adcb632aedd074a546397b9
Nathan <nchiu24@uwo.ca>
2025-10-26 23:14:03 -0400

    adding callbacks code

--- my-app/pages/api/auth/[...nextauth].ts
+++ my-app/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,19 @@ export const authOptions: NextAuthOptions = {
       }
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        ;(token as any).accessToken = account.access_token as string
+      }
+      return token
+    },
+    async session({ session, token }) {
+      // Send properties to the client, like an access_token from a provider.
+      ;(session as any).accessToken = (token as any).accessToken
+      return session
+    }
+  }
 }
-
 export default NextAuth(authOptions)
